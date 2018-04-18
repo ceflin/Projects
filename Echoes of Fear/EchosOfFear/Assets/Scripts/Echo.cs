d269599379fd530caa484509c54f9d6bbd467d29
svc-student <svc-student@student.directory.jccc>
2018-04-18 17:50:52 -0500

    More for echos

---
+++ Echoes of Fear/EchosOfFear/Assets/Scripts/Echo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Echo : MonoBehaviour
+{
+
+    public float time = 2.0f;
+    public GameObject echo;
+
+    private bool disableEcho = false;
+    public float disableDuration = 7.0f;
+    
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!disableEcho)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                GameObject echoCopy = Instantiate(echo, transform.position + (transform.forward * 2), transform.rotation);
+                Destroy(echoCopy, time);
+                disableEcho = true;
+                Invoke("EnableEcho", disableDuration);
+            }
+        }
+    }
+
+    void EnableEcho()
+    {
+        disableEcho = false;
+    }
+}
---
+++ Echoes of Fear/EchosOfFear/Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    private int playerHealth;
+    private int maxPlayerHealth;
+
+    private int enemyHealth;
+    private int maxEnemyHealth;
+
+
+    private void TakeDamage(int damage)
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
